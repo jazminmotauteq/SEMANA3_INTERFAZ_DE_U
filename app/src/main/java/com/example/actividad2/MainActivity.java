45ebc154fdc4e4d5b8d5e9a216a38b36ccd28d15
jazminmotauteq <125316302+jazminmotauteq@users.noreply.github.com>
2023-06-12 14:31:09 -0500

    segunda parcatica

--- app/src/main/java/com/example/actividad2/MainActivity.java
+++ app/src/main/java/com/example/actividad2/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.actividad2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,17 +20,48 @@ public class MainActivity extends AppCompatActivity {
     }
     public void btEnviar(View view)
     {
+        //para sacar la referencia a los controles
         EditText txtNombre = (EditText)findViewById(R.id.txtnombre);
         EditText txtApellido = (EditText)findViewById(R.id.txtapellido);
         RadioButton rbMasc = (RadioButton) findViewById(R.id.rbmasculino);
+        Switch swNotificaciones = (Switch) findViewById(R.id.swalerta);
+        CalendarView txtfecha = (CalendarView)findViewById(R.id.txtcalendario);
         String genero;
+        String nombre;
+        String apellido;
+        String notificaciones;
+        String  fecha;
+
         if(rbMasc.isChecked())
             genero = " Estimado";
         else
             genero = " Estimada";
+        //para sacar la informacion de los controles
+        nombre =  txtNombre.getText().toString();
+        apellido =  txtApellido.getText().toString();
+        notificaciones = swNotificaciones.isChecked()?"SI":"NO";
+        fecha = txtfecha.toString();
+
+        //mostrar mensaje con la informacion
         Toast.makeText(this.getApplicationContext(),
-                "hola" + genero + "" + txtNombre.getText().toString()
-                + "" + txtApellido.getText().toString(),
+                "hola " + genero + " " +nombre
+                + "" + apellido + "notificaciones " +notificaciones,
                 Toast.LENGTH_LONG). show();
+        //este es para habrir una actividad
+        Intent intent = new Intent(MainActivity.this, actividad3.class);
+        Bundle b = new Bundle();
+        //para enviar la informacion a la otra actividad
+        b.putString("NOMBRE", nombre);
+        b.putString("APELLIDO", apellido);
+        b.putString("NOTIFICACIONES", notificaciones);
+        b.putString("GENERO", genero);
+        b.putString("FECHA", fecha);
+
+//Añadimos la información al intent
+        intent.putExtras(b);
+// Iniciamos la nueva actividad
+        startActivity(intent);
+
+
     }
 }
--- app/src/main/java/com/example/actividad2/actividad3.java
+++ app/src/main/java/com/example/actividad2/actividad3.java
@@ -3,6 +3,7 @@ package com.example.actividad2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class actividad3 extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class actividad3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad3);
+        TextView txtSaludo = (TextView)findViewById(R.id.txtmensaje);
+//Recuperamos la información pasada en el intent
+        Bundle bundle = this.getIntent().getExtras();
+//Construimos el mensaje a mostrar
+        txtSaludo.setText("Hola!,  " +
+                bundle.getString("GENERO") +
+                " \n " +
+                bundle.getString("NOMBRE") +
+                " " +  bundle.getString("APELLIDO") +
+                " \n Recibe Notificaciones " +
+                bundle.getString("NOTIFICACIONES")
+                + " \n. se ha creado correctamente el contacto " +
+
+                bundle.getString("FECHA")
+        );
+
     }
 }
